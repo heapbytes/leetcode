377333d959bd6f95e740e6fb100933fe92c55e9f
Gourav Suram <heapbytes@pm.me>
2023-08-29 00:21:57 +0530

    Time: 5 ms (22.08%), Space: 5.9 MB (54.13%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.c
@@ -0,0 +1,13 @@
+int removeElement(int* nums, int numsSize, int val){
+
+  int cnt = 0;
+  for(int i=0; i<numsSize; i++){
+      if (nums[i] != val){
+          nums[cnt] = nums[i];
+          cnt++;        
+      }
+  }
+
+  return cnt;
+
+}
