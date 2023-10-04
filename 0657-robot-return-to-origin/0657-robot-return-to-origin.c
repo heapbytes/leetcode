eee997f0341484f328641b01c2f8337d4fc9b549
Gourav Suram <heapbytes@pm.me>
2023-10-04 16:52:18 +0530

    Time: 67 ms (9.52%), Space: 7 MB (8.84%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.c
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.c
@@ -1,29 +1,31 @@
 bool judgeCircle(char * moves){
   
-  int u = 0;
-  int d = 0;
-  int r = 0;
-  int l = 0;
+  int side = 0;
+  int vert = 0;
   
   for(int i=0; i<strlen(moves); i++){
     
     if (moves[i] == 'U'){
-      u += 1;
+      vert += 1;
     }
     
     else if (moves[i] == 'D'){
-      d += 1;
+      vert -= 1;
     }    
     
     else if (moves[i] == 'L'){
-      l += 1;
+      side -= 1;
     }    
     
     else if (moves[i] == 'R'){
-      r += 1;
+      side += 1;
     }
   }
   
-  return l == r && u == d;
-
+  
+  if(side || vert){
+    return false;
+  }
+  
+  return true;
 }
